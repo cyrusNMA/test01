48c9f7c19e5573d4e82feef071fde13951d3eb13
Cyrus <cyrus1127@gmail.com>
2016-01-06 00:33:32 +0800

    Updated Landing view
    Added rolling background.
    Removed non-use labels.
    Added Loading

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -27,62 +27,73 @@ bool HelloWorld::init()
         return false;
     }
     
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    visibleSize = Director::getInstance()->getVisibleSize();
+    origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+    auto background_rect = DrawNode::create();
+    background_rect->drawSolidRect(origin, visibleSize, Color4F(1, 0.5, 0, 1));
+    background_rect->setPosition(Vec2::ZERO);
+    this->addChild(background_rect);
     
-    // add a "start" lable to change scene
-    auto startLableItem = MenuItemLabel::create(cocos2d::Label::createWithTTF("START GAME", "fonts/Marker Felt.ttf", 50), CC_CALLBACK_1(HelloWorld::startGameCallBack, this));
-    
+    //add a start button
+    auto startLableItem = MenuItemLabel::create(cocos2d::Label::createWithTTF("START", "fonts/Marker Felt.ttf", 25), CC_CALLBACK_1(HelloWorld::startGameCallBack, this));
     startLableItem->setPosition(Vec2(origin.x + visibleSize.width/2 ,
                                 origin.y + visibleSize.height/2));
     
+    auto lab_version = MenuItemLabel::create(cocos2d::Label::createWithTTF("Version : 0.1.5", "fonts/Marker Felt.ttf", 5));
+    lab_version->setPosition(Vec2(origin.x + 10 ,origin.y + 10));
+    
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
+                                           "menu_normal.png",
+                                           "menu_selected.png",
                                            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
     
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width ,
+                                origin.y + closeItem->getContentSize().height));
 
     // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, startLableItem, NULL);
+    auto menu = Menu::create(closeItem, startLableItem, lab_version, NULL);
     menu->setPosition(Vec2::ZERO);
+    menu->setName("menu");
     this->addChild(menu, 1);
 
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
+//    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+//    
+//    // position the label on the center of the screen
+//    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+//                            origin.y + visibleSize.height - label->getContentSize().height));
+//
+//    // add the label as a child to this layer
+//    this->addChild(label, 1);
 
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
     
     
     //player BGM
     CocosDenshion::SimpleAudioEngine::sharedEngine()->playBackgroundMusic("sounds/noname_01.mp3", true);
+    
 
+    //set the rolling background object
+    {
+        
+        auto cnt_row = (int)(visibleSize.height / sp_egde_leng) + sp_spare_;
+        auto cnt_collum = (int)(visibleSize.width / sp_egde_leng) + sp_spare_;
+        
+        for(int sp_idx = 0 ; sp_idx < cnt_row * cnt_collum ; sp_idx++){
+            
+            auto n_sp = Sprite::create("res/heart_64.png");
+            n_sp->setPosition( Vec2(
+                                    (int)(sp_idx % cnt_collum) * sp_egde_leng,
+                                    (int)(sp_idx / cnt_collum) * sp_egde_leng ) );
+            this->addChild(n_sp);
+            bg_sp.push_back(n_sp);
+        }
+        
+        this->schedule(schedule_selector(HelloWorld::RollingBackground));
+        
+    }
+    
     return true;
 }
 
@@ -90,7 +101,7 @@ bool HelloWorld::init()
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+//	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
     return;
 #endif
 
@@ -103,9 +114,53 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
 
 void HelloWorld::startGameCallBack(Ref *pSender)
 {
-    MessageBox("You pressed the start button. Windows Store Apps do not implement a close button.","Alert");
+//    MessageBox("You pressed the start button. Windows Store Apps do not implement a close button.","Alert");
+
+    auto loading_rect = DrawNode::create();
+    loading_rect->drawSolidRect(origin, visibleSize, Color4F(0, 0, 0, 1));
+    loading_rect->setPosition(Vec2::ZERO);
+    loading_rect->setName("loading_splash");
+    this->addChild(loading_rect);
     
-    SceneManager::getInstance()->Goto(SceneManager::vailScene::InGame);
+    auto loading_text = Label::createWithTTF("Loading", "fonts/Marker Felt.ttf", 25);
+    loading_text->setPosition(Vec2(visibleSize.width - loading_text->getContentSize().width - 10, 0 + loading_text->cocos2d::Node::getContentSize().height + 10));
+    loading_text->setName("loading_text");
+    this->addChild(loading_text);
+    
+    this->removeChildByName("menu");
     
     CocosDenshion::SimpleAudioEngine::sharedEngine()->stopBackgroundMusic();
+    
+    this->scheduleOnce(schedule_selector(HelloWorld::changeSceneCallBack), 3);
+    
 }
+
+void HelloWorld::changeSceneCallBack(float dt)
+{
+    SceneManager::getInstance()->Goto(SceneManager::vailScene::InGame);
+}
+
+void HelloWorld::RollingBackground(float dt)
+{
+    auto cnt_row = (int)(visibleSize.height / sp_egde_leng) + sp_spare_;
+    auto cnt_collum = (int)(visibleSize.width / sp_egde_leng) + sp_spare_;
+    
+    if( bg_sp.size() ){
+        for(int sp_idx = 0 ; sp_idx < bg_sp.size() ; sp_idx++){
+            auto cur_pos = bg_sp[sp_idx]->getPosition();
+            
+            cur_pos.x -= bg_sp_mvsp * dt;
+            if( cur_pos.x + sp_egde_leng < 0 )
+                cur_pos.x = (cnt_collum - 1) * sp_egde_leng ;
+            
+            cur_pos.y -= bg_sp_mvsp * dt;
+            if( cur_pos.y + sp_egde_leng < 0 )
+                cur_pos.y = (cnt_row - 1) * sp_egde_leng;
+            
+            bg_sp[sp_idx]->setPosition(cur_pos);
+        }
+    }
+    
+}
+
+
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -7,6 +7,14 @@
 
 class HelloWorld : public cocos2d::Layer
 {
+    cocos2d::Size visibleSize;
+    cocos2d::Vec2 origin;
+    
+    int sp_egde_leng = 64;
+    int bg_sp_mvsp = 50;                    //move speed
+    int sp_spare_ = 3;
+    std::vector<cocos2d::Sprite*> bg_sp;    //rolling background sprite list
+    
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
@@ -17,6 +25,10 @@ public:
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     void startGameCallBack(cocos2d::Ref* pSender);
+    void changeSceneCallBack(float dt);
+    
+    //Background Roller
+    void RollingBackground(float dt);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
